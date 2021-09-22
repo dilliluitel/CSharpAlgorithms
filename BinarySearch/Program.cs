78af421f2ad0b1da568c2ee374d0d73fd168c9ab
Dilli Luitel <dilliluitel@gmail.com>
2021-09-21 19:30:22 -0500

    Add C# Algorithms Project files
    1. Added LinkedLists projects
    2. Added Stack projects
    3. Added Queue projects
    4. Added BinarySearch projects
    5. Added LinearSearch projects

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using static System.Console;
+
+/// <summary>
+/// Here we search an entire sorted array.
+/// </summary>
+public class Program
+{
+    public static void Main()
+    {
+        string[] dinners = {"Pasta",
+                              "Apple Pie",
+                              "Tuna Melt",
+                              "Mushroom Omelet",
+                              "Duck",
+                              "Eggplant"};
+
+        WriteLine();
+        WriteLine("Content of array:");
+        WriteLine("------------------");
+        foreach (string dinner in dinners)
+        {
+            WriteLine(dinner);
+        }
+
+        WriteLine("\nContent after Sort:");
+        WriteLine("------------------");
+        Array.Sort(dinners);
+
+        foreach (string dinner in dinners)
+        {
+            WriteLine(dinner);
+        }
+
+        WriteLine("\nBinarySearch for 'Beet Salad':");
+        int index = Array.BinarySearch(dinners, "No");
+        //Console.WriteLine(index);   //printing to see the negative index values
+        DislpayWhere(dinners, index);
+
+        WriteLine("\nBinarySearch for 'Tuna Melt':");
+        index = Array.BinarySearch(dinners, "Tuna Melt");
+        DislpayWhere(dinners, index);
+    }
+
+    private static void DislpayWhere<T>(T[] array, int index)
+    {
+        if (index < 0)
+        {
+            //Console.WriteLine(index);  //test prints
+            index = ~index;    //negates the negative signed bits
+            //Console.WriteLine(index);
+
+            Write("Not found. Sorts between: ");
+
+            if (index == 0)
+                Write("beginning of array and ");
+            else
+                Write("{0} and ", array[index - 1]);
+
+            if (index == array.Length)
+                WriteLine("end of array.");
+            else
+                WriteLine("{0}.", array[index]);
+        }
+        else
+        {
+            WriteLine("Found at index {0}.", index);
+        }
+    }
+}
